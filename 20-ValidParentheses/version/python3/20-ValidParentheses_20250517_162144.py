0f263fffa30d983e061814fb0552e9f9218695cc
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-05-17 16:21:44 -0700

    Create file

---
+++ 20-ValidParentheses/version/python3/20-ValidParentheses_20250517_162144.py
@@ -0,0 +1,24 @@
+# Last updated: 5/17/2025, 4:21:44 PM
+'''
+Time Complexity: O(n)
+Space Complexity: O(n)
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # check if string contains open to close 
+        # stack 
+        opens = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+        for charac in s: 
+            if charac in closeToOpen: 
+                # if top element is close's corresponding open
+                if opens and opens[-1] == closeToOpen[charac]:
+                    opens.pop()
+                else: 
+                    return False
+            else: 
+                opens.append(charac)
+
+        return len(opens) == 0
+                
