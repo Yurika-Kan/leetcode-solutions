e9f6dd8725161f2a81ca10a5b0b3a38a5da7139e
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-05-18 14:18:47 -0700

    File updated at 5/18/2025, 2:18:47 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,7 +1,22 @@
-# Last updated: 5/18/2025, 2:13:56 PM
+# Last updated: 5/18/2025, 2:18:47 PM
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+    def isAnagram(self, s: str, t: str) -> bool:
+        #check if s and t contain same letter makeup
+        #create freq map for s then t, check if s == t
+        smakeup = {}
+        tmakeup = {}
+        for char in s: 
+            # get curr count or default 0 to increment
+            smakeup[char]=smakeup.get(char, 0)+1
+        for char in t: 
+            tmakeup[char]=tmakeup.get(char, 0)+1
+        return smakeup == tmakeup 
 
-# Time Complexity: O(n)
-# Space Complexity: O(n)    
+        # s = '', t = '' --> true 
+        # s = '', t = 'a' --> false
+        # s = 'ss', t = 's' --> false 
+        # s = 'hello', t = 'oellh' --> true
+
+        # Time Complexity: O(n+m)
+        # Space Complexity: O(n+m)
+        
