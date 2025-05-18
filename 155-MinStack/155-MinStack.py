18268a4159366f0c050c49403d3360e7dc3a3e8c
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-05-18 14:11:30 -0700

    File updated at 5/18/2025, 2:11:29 PM

--- 155-MinStack/155-MinStack.py
+++ 155-MinStack/155-MinStack.py
@@ -1,8 +1,7 @@
-# Last updated: 5/18/2025, 2:10:35 PM
+# Last updated: 5/18/2025, 2:11:29 PM
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
 
 # Time Complexity: O(n)
-# Space Complexity: O(n)
-         
+# Space Complexity: O(n)    
