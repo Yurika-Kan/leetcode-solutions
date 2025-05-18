55ec415284a685e3a9c8db734e9cb254aa732980
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-05-18 14:19:12 -0700

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,22 @@
+# Last updated: 5/18/2025, 2:19:12 PM
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #check if s and t contain same letter makeup
+        #create freq map for s then t, check if s == t
+        smakeup = {}
+        tmakeup = {}
+        for char in s: 
+            # get curr count or default 0 to increment
+            smakeup[char]=smakeup.get(char, 0)+1
+        for char in t: 
+            tmakeup[char]=tmakeup.get(char, 0)+1
+        return smakeup == tmakeup 
+
+        # s = '', t = '' --> true 
+        # s = '', t = 'a' --> false
+        # s = 'ss', t = 's' --> false 
+        # s = 'hello', t = 'oellh' --> true
+
+        # Time Complexity: O(n+m)
+        # Space Complexity: O(n+m)
+        
