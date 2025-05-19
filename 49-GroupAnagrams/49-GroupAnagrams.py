484ad6380fe1388fa94ee6e00b16ffabcb3bb3ce
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-05-18 18:26:37 -0700

    Create file

---
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -0,0 +1,20 @@
+# Last updated: 5/18/2025, 6:26:37 PM
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # freq map         
+        freqMap = {}
+        # iterate through strs:
+        for s in strs: 
+            # sorted makes the string into a list!!! --> convert it back to a str 
+            word = ''.join(sorted(s))
+            # if sorted not in freq map key, add it and append to empty value list
+            if word not in freqMap:
+                freqMap[word] = [s]
+            # else if in, add og word value to sorted word key  
+            else: 
+                freqMap[word].append(s)
+        # return each value of map in list 
+        return list(freqMap.values())
+
+# Time Complexity: O(nklogn)
+# Space Complexity: O()
