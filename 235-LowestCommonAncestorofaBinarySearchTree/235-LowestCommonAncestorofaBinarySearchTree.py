a54a1a30fb78ee95e32099afe40ba57833f1dd86
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-08-19 13:57:41 -0700

    Create 235-LowestCommonAncestorofaBinarySearchTree

---
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
@@ -0,0 +1,33 @@
+# Last updated: 8/19/2025, 1:57:41 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # U: find closest ancestor of the 2 nodes, find where the split occurs
+        # M: BST, < / >, recursion 
+        # can't be ancestor if in diff subtree
+        # left or right 
+        # root is always ancestor
+        curr = root
+        
+        # iterate till find result or tree ends
+        while curr: 
+            # both greater -> right subtree
+            if p.val > curr.val and q.val > curr.val: 
+                # point to right 
+                curr = curr.right
+            # both lesser -> left subtree
+            elif p.val < curr.val and q.val < curr.val: 
+                curr = curr.left
+            # found p or q
+            else: 
+                return curr
+        # go to deeper subtree. if not both in same subtree, found ancestor!
+    
+    #Time Complexity: O(log n) - height of tree
+    #Space Complexity: O(1) - no data structures
