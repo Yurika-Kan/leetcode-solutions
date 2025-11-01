33c3078780541a21dc871de12435364eca1eb7c3
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-11-01 16:59:20 -0400

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.py
@@ -0,0 +1,17 @@
+# Last updated: 11/1/2025, 4:59:21 PM
+class Solution:
+    def maxProfit(self, prices):
+        min_price = prices[0]
+        max_profit = 0
+        
+        for price in prices[1:]:
+            max_profit = max(max_profit, price - min_price)
+            min_price = min(min_price, price)
+            
+        return max_profit
+
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
