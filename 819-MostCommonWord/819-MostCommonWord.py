d96c9ab316e7736ba29f1926d5199bdd2d03ab0c
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-11-01 19:19:34 -0400

    File updated at 11/1/2025, 7:19:35 PM

--- 819-MostCommonWord/819-MostCommonWord.py
+++ 819-MostCommonWord/819-MostCommonWord.py
@@ -1,13 +1,48 @@
-# Last updated: 11/1/2025, 5:23:04 PM
-class Solution:
-    def mostCommonWord(self, para: str, ban: List[str]) -> str:
-        s = "".join([i.lower() if i.isalnum() else ' ' for i in para])
-        word = s.split()
-        cou = {}
-        ban_set = set(ban)
+# Last updated: 11/1/2025, 7:19:35 PM
+# Trie - prefix tree 
 
-        for i in word:
-            if i not in ban_set:
-                cou[i] = cou.get(i, 0) + 1
-                        
-        return max( cou.items() , key=operator.itemgetter(1) )[0]
+# TrieNode - node with children + end marker bool 
+# node = {'i', children:{}, True}
+class Word: 
+
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+# Trie - set root node & update to traverse
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Word()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for letter in word:
+            if letter not in node.children: 
+                node.children[letter] = Word()
+            node = node.children[letter]
+        node.isEnd = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            node = root
+            # recursive
+            for i in range(j, len(word)): 
+                letter = word[i]
+
+                if letter == '.':
+                    for child in node.children.values():
+                        if dfs(i + 1, child): 
+                            return True
+                    return False
+                # return node.children[].children[].isEnd
+                # iterative
+                if letter not in node.children: 
+                    return False 
+                node = node.children[letter]
+            return node.isEnd
+        return dfs(0, self.root)
+    
+# Time C: O(n)
+# Space C: O(t + n) where t is num of TrieNodes created in Trie
