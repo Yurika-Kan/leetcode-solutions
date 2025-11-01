7d940c6ac1759903a4c6615f79d0f8f58cf91c52
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-11-01 19:22:15 -0400

    File updated at 11/1/2025, 7:22:15 PM

--- 819-MostCommonWord/819-MostCommonWord.py
+++ 819-MostCommonWord/819-MostCommonWord.py
@@ -1,48 +1,91 @@
-# Last updated: 11/1/2025, 7:19:35 PM
-# Trie - prefix tree 
+# Last updated: 11/1/2025, 7:22:15 PM
+import re 
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        # Understand: given a str of characters w uppercase, lowercase, punctuations, return word that is most common & not banned
+        # lowercase all
+        # looking for word% -surrounded by other punctuation
 
-# TrieNode - node with children + end marker bool 
-# node = {'i', children:{}, True}
-class Word: 
-
-    def __init__(self):
-        self.children = {}
-        self.isEnd = False
-
-# Trie - set root node & update to traverse
-class WordDictionary:
-
-    def __init__(self):
-        self.root = Word()
-
-    def addWord(self, word: str) -> None:
-        node = self.root
-        for letter in word:
-            if letter not in node.children: 
-                node.children[letter] = Word()
-            node = node.children[letter]
-        node.isEnd = True
+        # edge: 
+        # no most common word -> ""
         
+        # Match: splicing, lower, alnum, keys
+        # cleans paragaph to not include punctuality 
+        # over each char
+        cleanedP = "".join([char.lower() if char.isalnum() else ' ' for char in paragraph])
+        # default split chars to words
+        words = cleanedP.split()
+        freqMap = {}
 
-    def search(self, word: str) -> bool:
-        def dfs(j, root):
-            node = root
-            # recursive
-            for i in range(j, len(word)): 
-                letter = word[i]
+        for word in words:
+            # r u banned? if not join us!
+            if word not in banned:
+                freqMap[word] = freqMap.get(word, 0) + 1
+        
+        # return the key, val item with highest val (freq)
+        maxPair = max(freqMap.items(), key=lambda item: item[1]) 
 
-                if letter == '.':
-                    for child in node.children.values():
-                        if dfs(i + 1, child): 
-                            return True
-                    return False
-                # return node.children[].children[].isEnd
-                # iterative
-                if letter not in node.children: 
-                    return False 
-                node = node.children[letter]
-            return node.isEnd
-        return dfs(0, self.root)
-    
-# Time C: O(n)
-# Space C: O(t + n) where t is num of TrieNodes created in Trie
+        return maxPair [0]
+        
+# Time C: O(n + m) where n is len of words in paragraph & m is len of words in banned
+# Space C: O(n)
+
+'''
+one pass solution
+# one pass processing each char with most common accumulator
+        # case 1: middle of word ->  wadd word to word buffer
+        # case 2: middle of words in between (space or punc) -> check if word in banned, update freq of word, update most common word so far
+        res = ""
+        maxCount = 0
+        wordCount = {}
+        wordBuffer = []
+
+        for idx, char in enumerate(paragraph):
+            # char -> word 
+            # r u punctuation?
+            if char.isalnum():
+                # ok, let's lowercase u
+                wordBuffer.append(char.lower())
+                if idx != len(paragraph) - 1: 
+                    continue
+            
+            # u r space / punc
+            if len(wordBuffer) > 0: 
+                word = "".join(wordBuffer)
+                if word not in banned:
+                    wordCount[word] = wordCount.get(word, 0) + 1
+                    if wordCount[word] > maxCount: 
+                        maxCount = wordCount[word]
+                        res = word
+                # reset for next round
+                wordBuffer = []
+            
+        return res
+
+# Time C: O(n + m) where n is len of paragraph, m is len of banned
+# Space C: O()
+
+naive solution
+        # paragraph -> splice by ' ' -> list of items - lowercase, punctuality (just remove)
+        #paragraph.splice()
+        words = re.findall(r'\w+|[.,!?;]', paragraph)
+        #words.removeAll
+        punc = ["!", "?" ,"," , ";", "."]
+        for word in words: 
+            if word in punc: 
+                words.remove(word)
+        # dict - counter import - freq map 
+        freqMap = {}
+        for word in words: 
+            word = word.lower()
+            if word not in banned:
+                freqMap[word] = freqMap.get(word, 0)+1
+        
+        # sort it descending 
+        sortedWords = sorted(freqMap.items(), key=lambda item: item[1], reverse=True)
+        # iterate, return if not in banned 
+
+        return sortedWords[0][0]
+
+'''
+
