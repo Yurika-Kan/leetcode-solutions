e52f491f1c2271b58b6d39868c76778bfc19717f
Yurika Kan <148014074+Yurika-Kan@users.noreply.github.com>
2025-11-01 16:59:10 -0400

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,25 @@
+# Last updated: 11/1/2025, 4:59:10 PM
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # search through a sorted unique list for target #
+        # set pointers
+        left = 0 
+        right = len(nums) - 1
+
+        # while traversing list 
+        while left <= right: 
+            # find middle point
+            mid = (left+right) // 2
+            if target == nums[mid]:
+                return mid
+            # number is in low left half 
+            elif target < nums[mid]: 
+                right = mid - 1
+            # number is in high right half 
+            else: 
+                left = mid + 1
+        
+        return -1
+
+# Time Complexity: O(logn)
+# Space Complexity: O(1)
